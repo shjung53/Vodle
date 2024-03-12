674b175a00cd83de7628415289c4cc9c30883869
정수현 <gcjung777@naver.com>
2024-03-12 14:53:27 +0900

    feat: add main mypage login screen
    screen 정의, 패키지 정리
    
    Change-Id: I17202bf18329c022f850c4da243f485f1604e523

--- AOS_repo/presentation/src/main/java/MainActivity.kt
+++ AOS_repo/presentation/src/main/java/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tes.vodle.theme.VodleTheme
+import com.tes.presentation.theme.VodleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ AOS_repo/presentation/src/main/java/mypage/MyPageScreen.kt
@@ -0,0 +1,3 @@
+package com.tes.presentation.mypage
+
+class MyPageScreen
