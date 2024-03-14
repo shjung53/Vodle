fb466b8e10821fef9c08b413ae67d46576c91881
정수현 <gcjung777@naver.com>
2024-03-14 14:36:00 +0900

    feat: LoginScreen ui
    로그인 스크린 ui 구현
    Change-Id: I3c6e5e6ba18cda253b05c61caa9270d7739c123f

--- AOS_repo/presentation/src/main/java/login/LoginScreen.kt
+++ AOS_repo/presentation/src/main/java/login/LoginScreen.kt
@@ -1,10 +1,61 @@
 package com.tes.presentation.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tes.presentation.R
 
 @Composable
 fun LoginScreen(onClickText: () -> Unit) {
-    TextButton(onClick = onClickText, content = { Text(text = "로그인") })
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFC8DCDC)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_panpare),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .fillMaxWidth()
+                .aspectRatio(1f)
+        )
+
+        Text(text = "Vodle", fontSize = 72.sp)
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Image(
+            modifier = Modifier
+                .width(280.dp)
+                .height(40.dp)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) { onClickText() },
+            painter = painterResource(id = R.drawable.naver_login_button),
+            contentDescription = null
+        )
+
+        Spacer(modifier = Modifier.weight(1.1f))
+    }
 }
