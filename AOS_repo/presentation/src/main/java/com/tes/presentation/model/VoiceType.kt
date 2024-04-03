cbac6ef51a77bdf5699ea6ed9f0945660cb43c8f
정수현 <gcjung777@gmail.com>
2024-04-03 18:01:07 +0900

    feat: add voiceType
    목소리 추가

--- AOS_repo/presentation/src/main/java/com/tes/presentation/model/VoiceType.kt
+++ AOS_repo/presentation/src/main/java/com/tes/presentation/model/VoiceType.kt
@@ -1,8 +1,13 @@
 package com.tes.presentation.model
 
 enum class VoiceType(val korean: String, val eng: String) {
-    ORIGINAL("내 목소리", "original"), MUNDO("문도", "mundo"),
+    ORIGINAL("내 목소리", "original"),
+    RALO("랄로", "ralo"),
+    MUNDO("문도", "mundo"),
+    ELONMUSK("일론머스크", "elonMusk"),
+    BLITZCRANK("블리츠크랭크", "blitzcrank"),
     OPTIMUSPRIME("옵티머스프라임", "optimusPrime"),
     TRUMP("트럼프", "trump"),
-    AHRI("아리", "ahri"), ELSA("엘사", "elsa"),
+    AHRI("아리", "ahri"),
+    ELSA("엘사", "elsa"),
 }
